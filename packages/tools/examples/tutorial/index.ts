c0a1a03f7063a8c0c5ced32d3d6209bc0f111011
hyeonukkang <hyeonuk.kang@vuno.co>
2022-10-24 14:29:57 +0900

    Manipulation-tool

--- packages/tools/examples/tutorial/index.ts
+++ packages/tools/examples/tutorial/index.ts
@@ -56,77 +56,67 @@ async function run() {
     SeriesInstanceUID:
       '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
     wadoRsRoot: 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb',
-    type: 'VOLUME',
+    type: 'STACK',
   });
 
   const content = document.getElementById('content');
 
-  const viewportGrid = document.createElement('div');
-  viewportGrid.style.display = 'flex';
-  viewportGrid.style.flexDirection = 'row';
+  const element = document.createElement('div');
 
-  // element for axial view
-  const element1 = document.createElement('div');
-  element1.style.width = '500px';
-  element1.style.height = '500px';
+  // Disable the default context menu
+  element.oncontextmenu = (e) => e.preventDefault();
+  element.style.width = '500px';
+  element.style.height = '500px';
 
-  // element for sagittal view
-  const element2 = document.createElement('div');
-  element2.style.width = '500px';
-  element2.style.height = '500px';
-
-  viewportGrid.appendChild(element1);
-  viewportGrid.appendChild(element2);
-
-  content.appendChild(viewportGrid);
+  content.appendChild(element);
 
   const renderingEngineId = 'myRenderingEngine';
   const renderingEngine = new RenderingEngine(renderingEngineId);
 
-  // note we need to add the cornerstoneStreamingImageVolume: to
-  // use the streaming volume loader
-  const volumeId = 'cornerStreamingImageVolume: myVolume';
+  const viewportId = 'CT_AXIAL_STACK';
 
-  // Define a volume in memory
-  const volume = await volumeLoader.createAndCacheVolume(volumeId, {
-    imageIds,
+  const viewportInput = {
+    viewportId,
+    element,
+    type: ViewportType.STACK,
+  };
+
+  renderingEngine.enableElement(viewportInput);
+
+  const viewport = renderingEngine.getViewport(viewportId);
+
+  viewport.setStack(imageIds);
+
+  viewport.render();
+
+  addTool(ZoomTool);
+  addTool(WindowLevelTool);
+
+  const toolGroupId = 'myToolGroup';
+  const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+
+  // Add tools to the ToolGroup
+  toolGroup.addTool(ZoomTool.toolName);
+  toolGroup.addTool(WindowLevelTool.toolName);
+
+  toolGroup.addViewport(viewportId, renderingEngineId);
+
+  // Set the windowLevel tool to be active when the mouse left button is pressed
+  toolGroup.setToolActive(WindowLevelTool.toolName, {
+    bindings: [
+      {
+        mouseButton: csToolsEnums.MouseBindings.Primary, // Left Click
+      },
+    ],
   });
 
-  const viewportId1 = 'CT_AXIAL';
-  const viewportId2 = 'CT_SAGITTAL';
-
-  const viewportInput = [
-    {
-      viewportId: viewportId1,
-      element: element1,
-      type: ViewportType.ORTHOGRAPHIC,
-      defaultOptions: {
-        orientation: Enums.OrientationAxis.AXIAL,
+  toolGroup.setToolActive(ZoomTool.toolName, {
+    bindings: [
+      {
+        mouseButton: csToolsEnums.MouseBindings.Secondary, // Right Click
       },
-    },
-    {
-      viewportId: viewportId2,
-      element: element2,
-      type: ViewportType.ORTHOGRAPHIC,
-      defaultOptions: {
-        orientation: Enums.OrientationAxis.SAGITTAL,
-      },
-    },
-  ];
-
-  renderingEngine.setViewports(viewportInput);
-
-  // Set the volume to load
-  volume.load();
-
-  setVolumesForViewports(
-    renderingEngine,
-    [{ volumeId }],
-    [viewportId1, viewportId2]
-  );
-
-  // Render the image
-  renderingEngine.renderViewports([viewportId1, viewportId2]);
+    ],
+  });
 }
 
 run();
